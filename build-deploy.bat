b98d7f3ba1a6b8dcc2f7d1d88e47444316791659
jrichardsz <jrichardsz.java@gmail.com>
2023-09-03 16:09:30 -0500

    fix word wrapp and post title

--- build-deploy.bat
+++
@@ -1,4 +0,0 @@
-cd <folder>
-git commit -a -m "Add New Post"
-git ftp push -u <username> -p <password> - ftp://cdn.address.com
-git ftp push -u u443914890 -p <password> - ftp://address.com
--- build-init.bat
+++
@@ -1,4 +0,0 @@
-cd <folder>
-git commit -a -m "Repost Everything"
-git ftp init -u <username> -p <password> - ftp://cdn.address.com
-git ftp init -u <username> -p <password> - ftp://address.com
